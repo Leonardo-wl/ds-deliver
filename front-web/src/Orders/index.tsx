7731e676656814db897a50caa6890c0ac3d38327
Leonardo-wl <leonardo.alonsot@hotmail.com>
2021-02-17 12:50:03 -0300

    Dynamically receiving data from API

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { fecthProducts } from './api';
 import StepsHeader from './StepsHeader';
-import './styles.css';
+import { Product } from './types';
+import ProductsList from './ProductsList';
 
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+    //const [orderLocation, setOrderLoctation] = useState<OrderLocationdata>();
+
+    useEffect(() => {
+        fecthProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+    }, []);
+
     return(
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products} />            
         </div>
     )
 }
